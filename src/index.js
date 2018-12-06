85d1f4f02e208984439a146f49599f3562fdb7c8
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-06 20:14:46 +0530

    Update index file for client

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/scss/bootstrap/bootstrap-grid.scss';
 
-import './assets/scss/index.scss';
+import './assets/scss/index.module.scss';
+
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
