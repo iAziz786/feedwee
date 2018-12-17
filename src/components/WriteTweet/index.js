0129c7f39473fd806d066af39a5cd844ecbdd95e
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-17 21:45:32 +0530

    Add way to post tweet

--- src/components/WriteTweet/index.js
+++ src/components/WriteTweet/index.js
@@ -1,24 +1,51 @@
-import React, { PureComponent } from "react";
-import ContentEditable from "react-sane-contenteditable";
+import React, { PureComponent } from 'react';
+import axios from 'axios';
+import ContentEditable from 'react-sane-contenteditable';
 
-import Tweet from "../Buttons/Tweet";
+import Tweet from '../Buttons/Tweet';
 
-import WriteTweetStyle from "./WriteTweet.module.scss";
+import WriteTweetStyle from './WriteTweet.module.scss';
 
-class WriteTweet extends PureComponent {
+class WriteTweet extends React.Component {
+  state = {
+    tweetBody: ''
+  };
+  updateTweetBody = (event, value) => {
+    this.setState(() => ({
+      tweetBody: value
+    }));
+  };
+  tweetPost = async () => {
+    try {
+      await axios.post('/api/tweet/new', {
+        tweetBody: this.state.tweetBody
+      });
+      console.log('then');
+    } catch (err) {
+      console.log(err);
+    }
+    this.setState(
+      () => ({
+        tweetBody: ''
+      }),
+      () => this.forceUpdate(() => console.log('called forceupdate'))
+    );
+    // this.forceUpdate(() => console.log('called forceupdate'));
+  };
   render() {
     return (
       <div className={`${WriteTweetStyle.WriteTweet} py-2 px-3`}>
         <ContentEditable
-          className={`${WriteTweetStyle["text-area"]}`}
-          placeholder={"Write your tweet"}
+          className={`${WriteTweetStyle['text-area']}`}
+          placeholder={'Write your tweet'}
+          onChange={this.updateTweetBody}
           multiLine
         />
-        <div
-          className={'d-flex'}
-        >
-          <div></div>
-          <Tweet className={'ml-auto'}>Tweet</Tweet>
+        <div className={'d-flex'}>
+          <div />
+          <Tweet className={'ml-auto'} onClick={this.tweetPost}>
+            Tweet
+          </Tweet>
         </div>
       </div>
     );
