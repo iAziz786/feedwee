b341c213b2464994a063fc1baf1b77010e29d13f
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-17 21:48:02 +0530

    Add TweetFooter component

--- src/components/TweetFooter/index.js
+++ src/components/TweetFooter/index.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import { FiMessageCircle, FiHeart, FiRepeat } from 'react-icons/fi';
 
 import text from '../../assets/scss/_text.module.scss';
+import TweetFooterStyle from './TweetFooter.module.scss';
 
 const TweetFooter = React.memo(function TweetFooter(props) {
   const [heartSelected, setHeartState] = useState(false);
   return (
-    <div className={`d-flex justify-content-around ${text.small}`}>
-      <FiMessageCircle size={18} />
-      <FiRepeat size={18} />
+    <div className={`d-flex mt-2 justify-content-around ${text.small}`}>
+      <FiMessageCircle size={18} stroke={TweetFooterStyle.secondaryColor} />
+      <FiRepeat size={18} stroke={TweetFooterStyle.secondaryColor} />
       <FiHeart
-        onClick={() => setHeartState(!heartSelected)}
+        onClick={event => {
+          event.stopPropagation();
+          setHeartState(!heartSelected);
+        }}
         size={18}
+        stroke={TweetFooterStyle.secondaryColor}
         {...heartSelected && { fill: '#e0245e', stroke: '#e0245e' }}
       />
     </div>
