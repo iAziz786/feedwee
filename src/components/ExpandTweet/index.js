1ab8b4dfe7aaabcf18bb33e926797b339e83fe6f
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-17 21:13:08 +0530

    Add goBack when press escape key

--- src/components/ExpandTweet/index.js
+++ src/components/ExpandTweet/index.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { Card } from 'react-hover-card';
+import Feed from '../Feed';
 
-import Feed from "../Feed";
-
-import ExpandTweetStyle from "./ExpandTweet.module.scss";
+import ExpandTweetStyle from './ExpandTweet.module.scss';
 
 class ExpandTweet extends React.PureComponent {
+  componentDidMount() {
+    window.addEventListener('keydown', event => {
+      if (event.keyCode === 27) {
+        this.props.history.goBack();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', () => {});
+  }
   render() {
+    const {
+      match: {
+        params: { feedId }
+      },
+      history: { goBack }
+    } = this.props;
     return (
-      <div className={`${ExpandTweetStyle.Expand}`}>
-        <div className={`${ExpandTweetStyle["float-content"]}`}>
-          <Feed
-            feed={[
-              {
-                id: "sdifoasf",
-                content: "Some aghsdgContent",
-                author: "someone"
-              }
-            ]}
+      <div className={`${ExpandTweetStyle.Expand}`} onClick={goBack}>
+        <div className={`${ExpandTweetStyle['close-button']}`}>
+          <FaTimes
+            onClick={e => {
+              e.stopPropagation();
+              goBack();
+            }}
+            size={27}
           />
+        </div>
+        <div className={`${ExpandTweetStyle['float-content']}`}>
+          <p>This is cool: {feedId}</p>
         </div>
       </div>
     );
