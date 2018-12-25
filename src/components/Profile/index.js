f3fc784501cbebdf15a854e1bd5cb9351dea29ed
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-25 16:26:22 +0530

    Add dummy Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class Profile extends PureComponent {
+  render() {
+    return <div className={'justify-content-center'}>Profile page</div>;
+  }
+}
+
+export default Profile;
