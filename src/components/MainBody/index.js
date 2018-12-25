28c459fd23cc48cb57ebcac8173d12d3750f56cb
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-25 12:04:22 +0530

    Update MainBody style

--- src/components/MainBody/index.js
+++ src/components/MainBody/index.js
@@ -4,12 +4,11 @@ import ProfileSummary from '../ProfileSummary';
 import Posts from '../Posts';
 import Suggestions from '../Suggestions';
 
-import MainStyle from './MainBody.module.scss';
 import '../../assets/scss/ProfileCard.scss';
 
 const MainBody = React.memo(function MainBody(props) {
   return (
-    <div className={`${MainStyle.Main} container-fluid d-flex`}>
+    <div className={'container-fluid d-flex'}>
       <ProfileSummary />
       <Posts />
       <Suggestions />
