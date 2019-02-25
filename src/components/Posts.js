f14d8abc45095f2d87554bc22b45e5ebe235ed28
Mohammad Aziz <mdaziz067@gmail.com>
2019-02-25 23:53:07 +0530

    Update Posts component with Feeds

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
 import Loadable from 'react-loadable';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
 
-const Feeds = Loadable.Map({
-  loader: {
-    Feed: () => import('./Feed'),
-    feeds: () => axios.get('/api/tweets').then(res => res.data.tweets)
-  },
-  render(loaded, props) {
-    let Feed = loaded.Feed.default;
-    let feeds = loaded.feeds;
-    return feeds.map(feed => (
-      <Link
-        key={feed._id}
-        to={{
-          pathname: `/${feed.creator.username}/status/${feed._id}`,
-          // This is a treak!
-          state: { modal: true, feed }
-        }}
-        style={{ textDecoration: 'none', color: 'inherit' }}
-      >
-        <Feed key={feed._id} feed={feed} />
-      </Link>
-    ));
-  },
+const Feeds = Loadable({
+  loader: () => import('./Feeds'),
   loading: Loading
 });
 
