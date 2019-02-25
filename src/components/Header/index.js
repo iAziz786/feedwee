b488a5c818ae38f0b270116df6a8a68ed9ef05ea
Mohammad Aziz <mdaziz067@gmail.com>
2019-02-25 23:51:28 +0530

    Use style component for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,14 @@ import { FiHome, FiBell, FiMail, FiSearch } from 'react-icons/fi';
 
 import BrandLogo from '../BrandLogo';
 import BurgerIcon from '../BurgerIcon';
-import Dropdown from '../Dropdown';
+
+import {
+  MainWrapper,
+  FixWrapper,
+  ResponsiveItems,
+  Navbar,
+  Dropdown
+} from './style';
 
 import HeaderStyle from './Header.module.scss';
 
@@ -26,52 +33,38 @@ class Header extends PureComponent {
 
   render() {
     return (
-      <div className={`d-flex ${HeaderStyle.Header}`}>
-        <div className={`d-flex ${HeaderStyle['Header-Inner']}`}>
-          <nav
-            className={
-              'container d-flex justify-content-between align-items-center'
-            }
-          >
-            <div
-              className={'row justify-content-between'}
-              style={{ width: '100%' }}
-            >
-              <div className={`d-flex col-4 justify-content-around`}>
-                <Link to="/">
-                  <FiHome className={HeaderStyle['icon-style']} />
-                </Link>
-                <Link to="/explore">
-                  <FiSearch className={HeaderStyle['icon-style']} />
-                </Link>
-                <Link to="/notifications">
-                  <FiBell className={HeaderStyle['icon-style']} />
-                </Link>
-                <Link to="/messages">
-                  <FiMail className={HeaderStyle['icon-style']} />
-                </Link>
+      <MainWrapper>
+        <FixWrapper>
+          <ResponsiveItems>
+            <Navbar>
+              <Link to="/">
+                <FiHome className={HeaderStyle['icon-style']} />
+              </Link>
+              <Link to="/explore">
+                <FiSearch className={HeaderStyle['icon-style']} />
+              </Link>
+              <Link to="/notifications">
+                <FiBell className={HeaderStyle['icon-style']} />
+              </Link>
+              <Link to="/messages">
+                <FiMail className={HeaderStyle['icon-style']} />
+              </Link>
+            </Navbar>
+            <Dropdown>
+              <div
+                className={`d-flex ${HeaderStyle['header-options']}`}
+                onClick={this.props.toggleDropdown}
+              >
+                {this.state.isLoggedIn ? (
+                  <a href="/logout">Logout</a>
+                ) : (
+                  <a href="/oauth/authorize">Login</a>
+                )}
               </div>
-              <div style={{ position: 'relative' }}>
-                <BurgerIcon
-                  className={'d-sm-block d-md-none'}
-                  onClick={this.props.toggleDropdown}
-                />
-                {/* <Dropdown show={this.props.showDropdown} /> */}
-                <div
-                  className={`d-flex ${HeaderStyle['header-options']}`}
-                  onClick={this.props.toggleDropdown}
-                >
-                  {this.state.isLoggedIn ? (
-                    <a href="/logout">Logout</a>
-                  ) : (
-                    <a href="/oauth/authorize">Login</a>
-                  )}
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
+            </Dropdown>
+          </ResponsiveItems>
+        </FixWrapper>
+      </MainWrapper>
     );
   }
 }
