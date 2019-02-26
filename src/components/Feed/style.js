2c144fabe210971fa3d6f1c4f32a21f156435632
Mohammad Aziz <mdaziz067@gmail.com>
2019-02-27 00:56:49 +0530

    Add some style for Feed component

---
+++ src/components/Feed/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SecondaryText = styled.p`
+  display: inline-block;
+  font-weight: normal;
+  color: rgb(136, 153, 166);
+`;
+
+export const Dot = styled(SecondaryText)`
+  padding: 5px;
+`;
