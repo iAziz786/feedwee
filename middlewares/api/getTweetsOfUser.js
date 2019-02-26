2cfb8f4ae305ba569bffc3f7ac6a8fc1ccf99484
Mohammad Aziz <mdaziz067@gmail.com>
2019-02-27 00:34:06 +0530

    return tweets in sorted order

--- middlewares/api/getTweetsOfUser.js
+++ middlewares/api/getTweetsOfUser.js
@@ -3,7 +3,11 @@ const { Tweet } = require('../../components/tweet/modal');
 async function getTweetsOfUser(req, res) {
   const { _id: userId } = req.user;
   try {
-    const tweets = await Tweet.find({ creator: userId }).populate('creator');
+    const tweets = await Tweet.find({
+      creator: userId
+    })
+      .sort({ createdAt: -1 })
+      .populate('creator');
     return res.status(200).json({ success: true, tweets });
   } catch (err) {
     return res.status(400).json({ success: false });
